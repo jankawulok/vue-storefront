d5e4b52cf3935d438b04f5744053d6a175e30a22
Jan <jan@iMac-Jan.local>
2020-07-15 17:57:45 +0200

    ps:api-client: use apiClientFactory, move settings to config

--- packages/prestashop/api-client/src/index.ts
+++ packages/prestashop/api-client/src/index.ts
@@ -1,3 +1,4 @@
+import { apiClientFactory } from '@vue-storefront/core';
 import ApolloClient from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory';
@@ -17,45 +18,42 @@ import fetch from 'isomorphic-fetch';
 
 let apolloClient: ApolloClient<any> = null;
 
-const locale = 'pl';
-const currency = 'PLN';
-const country = 'PL';
-const countries = [];
-const currencies = [];
-const locales = [];
 
-const setup = <TCacheShape>(setupConfig?: SetupConfig<TCacheShape>): ApolloClient<TCacheShape> => {
-
-  apolloClient = new ApolloClient({
-    link: createHttpLink({
-      uri: setupConfig.api.endpoint,
-      headers: {
-        credentials: 'include'
-      },
-      credentials: 'include',
-      fetch
-    }),
-    cache: new InMemoryCache({
-      dataIdFromObject: object => {
-        switch (object.__typename) {
-          case 'Product': return `Product:${(object as any).url_key}`;
-          case 'Category': return `Category:${(object as any).url_key}`;
-          default: defaultDataIdFromObject(object);
+const { setup, override, update, getSettings } = apiClientFactory<any, any>({
+  defaultSettings: {},
+  onSetup: <TCacheShape>(setupConfig: SetupConfig<TCacheShape>) => {
+    // todo: add possibility to override
+    apolloClient = new ApolloClient({
+      link: createHttpLink({
+        uri: setupConfig.api.endpoint,
+        headers: {
+          credentials: 'include'
+        },
+        credentials: 'include',
+        fetch
+      }),
+      cache: new InMemoryCache({
+        dataIdFromObject: object => {
+          switch (object.__typename) {
+            case 'Product': return `Product:${(object as any).url_key}`;
+            case 'Category': return `Category:${(object as any).url_key}`;
+            default: defaultDataIdFromObject(object);
+          }
         }
-      }
-    }),
-    defaultOptions: {
-      watchQuery: {
-        fetchPolicy: 'network-only'
-      }
-    },
-    ...setupConfig.customOptions
-  });
-  return apolloClient;
-};
+      }),
+      defaultOptions: {
+        watchQuery: {
+          fetchPolicy: 'network-only'
+        }
+      },
+      ...setupConfig.customOptions
+    });
+  }
+});
 
 export {
   apolloClient,
+  getSettings,
   getProduct,
   getCategory,
   getCart,
@@ -67,11 +65,5 @@ export {
   customerLogout,
   customerCreate,
   setup,
-  locale,
-  locales,
-  country,
-  countries,
-  currency,
-  currencies,
   getUrlResolver
 };
--- packages/prestashop/api-client/src/types/setup.ts
+++ packages/prestashop/api-client/src/types/setup.ts
@@ -3,10 +3,17 @@ import { ApolloClientOptions } from 'apollo-client';
 export interface ApiConfig {
   endpoint: string;
   scopes: string[];
+  imgEndpoint: string;
 }
 
 export interface SetupConfig<TCacheShape> {
   api?: ApiConfig;
   customOptions?: ApolloClientOptions<TCacheShape>;
+  currency?: string;
+  locale?: string;
+  country?: string;
+  countries?: LocaleItem[];
+  currencies?: LocaleItem[];
+  locales?: LocaleItem[];
 }
 
