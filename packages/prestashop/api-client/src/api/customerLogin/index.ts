f1f6e8b505dd21ea98c25a33ebfa462212f54d46
jankawulok <jan@kawulok.com.pl>
2020-10-05 14:23:01 +0200

    ps: update tsconfig

--- packages/prestashop/api-client/src/api/customerLogin/index.ts
+++ packages/prestashop/api-client/src/api/customerLogin/index.ts
@@ -1,8 +1,11 @@
-import { CustomerLoginResponse } from './../../types/Api';
+import { ApiCustomerLoginResponse } from './../../types/Api';
 import { apolloClient } from '../..';
 import defaultMutation from './defaultMutation';
 
-const customerLogin = async (input: {email: string; password: string}): Promise<CustomerLoginResponse> => {
+const customerLogin = async (input: {
+  email: string;
+  password: string;
+}): Promise<ApiCustomerLoginResponse> => {
   return await apolloClient.mutate({
     mutation: defaultMutation,
     variables: input,
