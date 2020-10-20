bcfb8ac3b07df28cb101df951487ec4ccf427fe1
jankawulok <jan@kawulok.com.pl>
2020-10-20 14:47:13 +0200

    chore: properly export types

--- packages/prestashop/composables/src/useUserOrders/factoryParams.ts
+++ packages/prestashop/composables/src/useUserOrders/factoryParams.ts
@@ -7,7 +7,7 @@ import { OrderSearchParams } from '../types';
 export const params: UseUserOrdersFactoryParams<Order, OrderSearchParams> = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   searchOrders: async (params: OrderSearchParams = {}) => {
-    const result = await getMe({params: { withOrders: true }});
+    const result = await getMe();
     const { result: data, total } = result.data?.customer.orders || { results: [], total: 0};
     return { data, total };
   }
